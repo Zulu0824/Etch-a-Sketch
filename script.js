bc423f8750a5f14ad6a0eb39bfea4074e59dfdc0
Zulu0824 <sriharsha.patta@gmail.com>
2025-10-13 02:23:02 +0530

    Add container to hold grid

---
+++ script.js
@@ -0,0 +1,13 @@
+let row = 16;
+let cols = row;
+const canvasSize = 800;
+let rgbMode = false;
+
+const container1 = document.createElement("div");
+document.body.appendChild(container1);
+
+container1.style.display = "flex";
+container1.style.flexWrap = "Wrap";
+container1.style.width = canvasSize + "px";
+container1.style.height = canvasSize + "px";
+container1.style.border = "2px solid black";
