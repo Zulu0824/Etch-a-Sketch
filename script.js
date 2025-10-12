45d06a746298b272ee0e7f7243cec1f2590e9c01
Zulu0824 <sriharsha.patta@gmail.com>
2025-10-13 02:27:55 +0530

    Add JS to HTML

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ let rgbMode = false;
 const container1 = document.createElement("div");
 document.body.appendChild(container1);
 
+// Container1 is our canvas for building and holding the grid
+
 container1.style.display = "flex";
 container1.style.flexWrap = "Wrap";
 container1.style.width = canvasSize + "px";
 container1.style.height = canvasSize + "px";
 container1.style.border = "2px solid black";
 
+// Container2 holds all of our buttons 
 const container2 = document. createElement("div");
 container2.id = "button-box";
 document.body.appendChild(container2);
+
