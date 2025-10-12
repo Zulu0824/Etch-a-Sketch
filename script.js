a318ce3a4fa59d4740a886e6c5058780c19bdbfc
Zulu0824 <sriharsha.patta@gmail.com>
2025-10-13 02:55:46 +0530

    Add Grid using DOM in JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let row = 16;
-let cols = row;
+let rows = 16;
+let cols = rows;
 const canvasSize = 800;
 let rgbMode = false;
 
@@ -9,7 +9,7 @@ document.body.appendChild(container1);
 // Container1 is our canvas for building and holding the grid
 
 container1.style.display = "flex";
-container1.style.flexWrap = "Wrap";
+container1.style.flexWrap = "wrap";
 container1.style.width = canvasSize + "px";
 container1.style.height = canvasSize + "px";
 container1.style.border = "2px solid black";
@@ -25,7 +25,25 @@ const clearBtn = document.getElementById("clearBtn");
 const canvasBtn = document.getElementById("canvasBtn");
 const rbgBtn = document.getElementById("rgbBtn");
 
-container2.appendChild("clearBtn");
-container2.appendChild("canvasBtn");
-container2.appendChild("rgbBtn");
+container2.appendChild(clearBtn);
+container2.appendChild(canvasBtn);
+container2.appendChild(rgbBtn);
 
+function buildGrid(rows, cols) {
+    container1.innerHTML="";
+    const cellSize = canvasSize / cols;
+
+    for (let i = 0; i < rows *cols; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = cellSize + "px";
+        cell.style.height = cellSize + "px";
+        cell.style.border = "1px solid black";
+        cell.style.backgroundColor = "white";
+        cell.style.boxSizing = "border-box";
+
+        container1.appendChild(cell);
+    }
+}
+
+buildGrid(rows, cols);
